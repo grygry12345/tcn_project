2356f285bfb7e5114aab2ecd932031c82e476f53
Giray Kilic <kilicgiray@outlook.com>
2022-05-08 18:47:15 +0300

    add dataset control for shorter load times

--- main.py
+++ main.py
@@ -1,43 +1,62 @@
 import torch
 from torch.utils.data import DataLoader
-import model as m  
 import torch.nn as nn
+import os
+import numpy as np
 
+import model as m  
 from dataset import HDF5Dataset
+import trainer as t
 
 if __name__ == '__main__':
     device = "cuda" if torch.cuda.is_available() else "cpu"
-    batch_size = 64
+    batch_size = 4
     lr = 1e-3
-    epochs = 100
+    epochs = 5
 
-    training_data = HDF5Dataset(file_path='data', group='train', file_name='avds000-lab010-01')
+    training_data = HDF5Dataset(file_path='data', group='train')
+    val_data = HDF5Dataset(file_path='data', group='val')
+
+    # if var folder not empty then load data and labels from saved numpy arrays
+    # else create data and labels
+    if not os.path.exists('data/var'):
+        os.mkdir('data/var')
+    
+    if not os.path.exists('data/var/data.npy'):
+        training_data.create_data()
+        val_data.create_data()
+    else:
+        print('Loading data...')
+        training_data.data = np.load('data/var/data.npy')
+        val_data.data = np.load('data/var/data.npy')
+        print('Data loaded.')
+
+    
+    if not os.path.exists('data/var/labels.npy'):
+        training_data.create_labels()
+        val_data.create_labels()
+    else:
+        print('Loading labels...')
+        training_data.labels = np.load('data/var/labels.npy')
+        val_data.labels = np.load('data/var/labels.npy')
+        print('Labels loaded.')
+    
+    training_data.save_varibles('data/var')
+
+
     training_loader = DataLoader(training_data, batch_size=batch_size, shuffle=True)
-    frame_number = training_loader.dataset.data.shape[0]
-    frame_features = training_loader.dataset.data.shape[1]
+    val_loader = DataLoader(val_data, batch_size=batch_size, shuffle=True)
 
-    model = m.DilatedResidualLayer(channels=4, output=5, dim=frame_features).to(device)
+    # frame_number = training_loader.dataset.data.shape[0]
+    frame_features_train = training_loader.dataset.data.shape[1]
+
+    model = m.DilatedResidualLayer(channels=1, output=1, dim=frame_features_train).to(device)
     
     loss_fn = nn.CrossEntropyLoss()
     optimizer = torch.optim.Adam(model.parameters(), lr=lr)
 
-    model.train()
-    for epoch in range(epochs):   
-        size = len(training_loader.dataset)
-        model.train()
-        for batch, (X, y) in enumerate(training_loader):
-            X = X.to(device) 
-            y = y.type(torch.LongTensor)
-            y = y.to(device)
-
-            pred = model(X)
-            loss = loss_fn(pred.squeeze(), y.squeeze())
-            
-
-            optimizer.zero_grad()
-            loss.backward()
-            optimizer.step()
-
-        print(f"Epoach {epoch+1} Loss: {loss.item()}")
-        
+    trainer = t.Trainer(model, loss_fn, optimizer, epochs=epochs, train_dataloader=training_loader, \
+                        val_dataloader=val_loader, printBatch=True, device=device)
+    trainer.train()
+           
         
