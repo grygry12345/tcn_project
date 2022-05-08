5203992ec9cef7001872e2d81beb391d9c3e1011
Giray Kilic <kilicgiray@outlook.com>
2022-05-08 18:48:33 +0300

    move plots script to misc file

--- misc.py
+++ misc.py
@@ -1,50 +1,36 @@
+"""
+Created by Arman Savran at 2022-04-16
+"""
+
 import numpy as np
-import h5py
-import plots
 import matplotlib.pyplot as plt
-import json
-
-def get_relative_path(data, directory):
-    keys = list(data.keys())
-    for i in range(len(data)):
-        keys[i] = directory + '/' + keys[i] + '.h5'
-    return keys
-
-def combine_dataset():
-    directory = 'data/roi_mouth'
-    height = 48
-    width = 64
-    channels = 2
-    total_frames = 0
-
-    # get sets json file
-    with open('data/sets.json') as f:
-        sets = json.load(f)
-
-    train_list = sets['train']
-    val_list = sets['validation']
-    test_list = sets['test']
-    
-    # get train data lists in sets
-    train_list_paths = get_relative_path(train_list, directory)
-    val_list_paths = get_relative_path(val_list, directory)
-    test_list_paths = get_relative_path(test_list, directory)
-    
-    train_y = train_list.values()
-    val_y = val_list.values()
-    test_y = test_list.values()
+from matplotlib import cm as mpcm
+import h5py
 
 
-    train_x = np.zeros((total_frames, channels, height, width))
-    for i in range(len(train_list_paths)):
-        with h5py.File(train_list_paths[i], 'r') as f:
-            data = f['data']
-            # append frames to train_x array
-            train_x = np.append(train_x, data, axis=0)
-            print("iteration: " + str(i) + "/" + str(len(train_list_paths)))
-    
-    print('train_x shape: ', train_x.shape)
+def plot_est_im(eventspiketensor, frame_size_ms, idx):
+    im = eventspiketensor[idx].sum(axis=0)
+    lim_intensity = frame_size_ms * 5e-2
+    fig, ax = plt.subplots()
+    ax.imshow(im, clim=(0, lim_intensity), origin='lower', interpolation='nearest', cmap=mpcm.Blues)
+    return fig, ax
 
+
+def plot_timeseq(eventspiketensor, frame_size_ms, segment, name):
+    event_activation = eventspiketensor.sum(axis=(1, 2, 3))
+    step = frame_size_ms * 1e-3  # Convert to second
+    t_offset = step / 2
+    t_frames = t_offset + step * np.arange(event_activation.shape[0])
+    fig, ax = plt.subplots(figsize=(10, 2))
+    ax.plot(t_frames, event_activation)
+    if segment is not None and (segment[0] >= 0 and segment[1] >= 0):
+        # print(segment)
+        ax.axvline(segment[0], c='k')
+        ax.axvline(segment[1], c='k')
+    ax.set_xlim((t_frames[0], t_frames[-1]))
+    max_y = event_activation.max()
+    ax.set_ylim((0, max_y))
+    return fig, ax
 
 def plot_axample():
     with h5py.File('data/roi_mouth/avds000-lab010-01.h5', 'r') as f:
@@ -52,5 +38,5 @@ def plot_axample():
         train_x = np.array(train_x)
     
     print(train_x.shape)
-    plots.plot_timeseq(train_x, 10, [1.1624487967229904, 3.7137179979518695], 'test')
+    plot_timeseq(train_x, 10, [1.1624487967229904, 3.7137179979518695], 'test')
     plt.show()
