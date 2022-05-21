2214ff62003a44ca579511bef7fb528228056d47
Giray Kilic <kilicgiray@outlook.com>
2022-05-21 14:57:37 +0300

    add step_size and frame_size

--- dataset.py
+++ dataset.py
@@ -1,20 +1,23 @@
 from lib2to3.pytree import Node
+import torch
 from torch.utils.data import Dataset
 import h5py
 import json
 import numpy as np
-import pickle
-# from eray.project import t_frames
+from torch.nn.functional import normalize
 
 class HDF5Dataset(Dataset):
-    def __init__(self, file_path='data', data_path="data/roi_mouth", transform=None, group='train'):
+    def __init__(self, file_path='data', data_path="data/roi_mouth", transform=None, group='train', \
+    frame_count: int = 3, step_size: int = 1, device = 'cpu'):
         self.file_path = file_path
         self.transform = transform
         self.group = group
-        self.data = None
-        self.labels = None
         self.data_path = data_path
-        # self._frame_segments = None
+        self.frame_count = frame_count
+        self.step_size = step_size
+        self.device = device
+        self.labels = None
+        self.data = None
         self._frame_lengths = {}
 
     def __len__(self):
@@ -29,74 +32,77 @@ class HDF5Dataset(Dataset):
 
     def _concatenate_frames(self, sets, group):
         i = 0
-        total_frame = 0
-        firstTime = True
         file_list = sets[group].keys()
         for file_name in file_list:
             with h5py.File(self.data_path + '/' + file_name + '.h5', 'r') as f:
                 d = f['data']
                 d = np.array(d)
-                d = d.reshape(d.shape[0], -1)
+                d = torch.tensor(d, dtype=torch.float32, device=self.device)
 
-                # video_length = d.shape[0]         
-                # if (firstTime):
-                #     frame_start = total_frame
-                #     firstTime = False 
-                # else:
-                #     frame_start = total_frame + 1
+                d = d.sum(axis=(1,2,3))
+                d_len = d.shape[0]
 
-                
-                # frame_end = video_length + total_frame
-                # total_frame += video_length
+                # normailize d
+                d = normalize(d, dim=0, p=2)
 
-                self._frame_lengths[file_name] = d.shape[0]
+
+                for frame in range(0, d_len, self.step_size):
+
+                    if frame + self.frame_count > d_len:
+                        break
+                    else:
+                        seq = d[frame:frame+self.frame_count]
+                        # seq = normalize(seq, dim=0)
+                        seq = seq.unsqueeze(0)
+
+                        if self.data is None:
+                            self.data = seq.clone()
+                        else:
+                            self.data = torch.cat((self.data, seq), dim=0)
                     
-                if self.data is None:
-                    self.data = d
-                    # self.data = {0: d}
-                    # self._frame_segments = [[frame_start, frame_end]]
-                else:
-                    self.data = np.concatenate((self.data, d))
-                    
-                    # data dictinoary append
-                    # self.data[i] = d
-                    # self._frame_segments = np.append(self._frame_segments, [[frame_start, frame_end]], axis=0)
-                    
+            self._frame_lengths[file_name] = d_len
+
             # progress bar
             i += 1
-
             print('Creating data {}/{}'.format(i, len(sets[group])), end='\r')
+        print()
+        
 
 
     def _concatenate_labels(self, sets, group):
         i = 0
-        for file_name in sets[group].keys():
+        file_list = sets[group].keys()
+        for file_name in file_list:
             segments = sets[group][file_name]
+            segments = np.array(segments)
+            segments = torch.tensor(segments, dtype=torch.float32, device=self.device)
             frame_length = self._frame_lengths[file_name]
 
-            if (segments[0] != -1 and segments[1] != -1):
-                l = np.zeros(frame_length)
+
+            if segments[0] == -1 and segments[1] == -1:
+                l = torch.zeros(frame_length, dtype=torch.float32, device=self.device)
+            else:
+                l = torch.zeros(frame_length, dtype=torch.float32, device=self.device)
                 start = int(segments[0] * 100)
                 end = int(segments[1] * 100)
                 l[start:end] = 1
-                # start = segments[0]
-                # end = segments[1]
-                # l = [[start, end]]
+            
+            for frame in range(0, frame_length, self.step_size):
+                if frame + self.frame_count > l.shape[0]:
+                    break
+                else:
+                    seq = l[frame:frame+self.frame_count]
+                    seq = seq.unsqueeze(0)
 
-            else:
-                l = np.zeros(frame_length)
-                #l = [[-1, -1]]
-            
-            # append l to self.labels
-            if self.labels is None:
-                self.labels = l
-            else:
-                self.labels = np.concatenate((self.labels, l))
-            
+                    if self.labels is None:
+                        self.labels = seq.clone()
+                    else:
+                        self.labels = torch.cat((self.labels, seq), dim=0)
+                
             # progress bar
             i += 1
-
             print('Creating labels {}/{}'.format(i, len(sets[group])), end='\r')
+        print()
 
 
     def create_data(self):
@@ -114,10 +120,8 @@ class HDF5Dataset(Dataset):
             else:
                 raise Exception('Invalid group')
                
-            # reshape after concanating
-            # self.data = self.data.reshape(self.data.shape[0], -1)
     
-    def create_labels(self, frame_size_ms=10): # ? Maybe add frame_size_ms for the labels
+    def create_labels(self):
         label_file_path = self.file_path + '/sets.json'
 
         # labels
@@ -131,50 +135,14 @@ class HDF5Dataset(Dataset):
                 self._concatenate_labels(segments, 'test')
             else:
                 raise Exception('Invalid group')
-  
-    def create_one_data(self, file_name):
-        with h5py.File(self.data_path + '/' + file_name + '.h5', 'r') as f:
-            self.data = np.array(f['data'])
-            self.data = self.data.reshape(self.data.shape[0], -1)
     
-    def create_one_labels(self, file_name, group):
-        with open(self.file_path + '/sets.json', 'r') as l:
-            labels = json.load(l)
-            if group == 'train':
-                segments = labels['train'][file_name]
-            elif group == 'val':
-                segments = labels['validation'][file_name]
-            elif group == 'test':
-                segments = labels['test'][file_name]
-            
-            
-            start = int(segments[0] * 100)
-            end = int(segments[1] * 100)
-            if (segments[0] != -1 and segments[1] != -1):
-                self.labels = np.zeros(self.data.shape[0])
-                self.labels[start:end] = 1
-            else:
-                self.labels = np.zeros(self.data.shape[0])
-            
-            #if (segments[0] != -1 and segments[1] != -1):
-            #     self.labels = [-1, -1]
-            # else:
-            #     start = segments[0]
-            #     end = segments[1]
-            #     self.labels = [start, end]
                        
     def load_variables(self, file_path, file_name_data, file_name_label):
-        self.data = np.load(file_path + file_name_data + '.npy') # data
-        # with open(file_path + file_name_data + '.pkl', 'rb') as f:
-        #     self.data = pickle.load(f)
-        self.labels = np.load(file_path + file_name_label + '.npy') # labels
-
-
+        self.data = torch.load(file_path + file_name_data + '.pt')
+        self.labels = torch.load(file_path + file_name_label + '.pt')
     # save data and labels to file
     def save_varibles(self, file_path, file_name_data, file_name_label):
-        np.save(file_path + file_name_data, self.data) # data
-        # with open(file_path + file_name_data + '.pkl' , 'wb') as f:
-        #     pickle.dump(self.data, f)
-        np.save(file_path + file_name_label, self.labels) # labels
+        torch.save(self.data, file_path + file_name_data + '.pt')
+        torch.save(self.labels, file_path + file_name_label + '.pt')
 
 
