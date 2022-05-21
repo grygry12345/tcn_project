483a2e6f11709fa43b8596173ba1c3be8ed6d493
Giray Kilic <kilicgiray@outlook.com>
2022-05-21 15:00:13 +0300

    remove else statement for one file

--- main.py
+++ main.py
@@ -11,73 +11,59 @@ import eval
 
 if __name__ == '__main__':
     device = "cuda" if torch.cuda.is_available() else "cpu"
-    batch_size = 512
-    epochs = 20
+    batch_size = 64
+    epochs = 100
     lr = 1e-6
-    num_channel = 2
+    step_size = 10
+    frame_count = 5
+    filter_size = 4
+    num_class = 2
 
-    onlyOneFile = False
-    file_train = "avds000-lab010-01"
-    file_val = "avds012-lab010-01"
-    file_test = "avds014-lab010-01"
-
-    training_data = HDF5Dataset(file_path='data', group='train')
-    val_data = HDF5Dataset(file_path='data', group='val')
-    test_data = HDF5Dataset(file_path='data', group='test')
+    training_data = HDF5Dataset(file_path='data', group='train', device=device, frame_count=frame_count, step_size=step_size)
+    val_data = HDF5Dataset(file_path='data', group='val', device=device, frame_count=frame_count, step_size=step_size)
+    test_data = HDF5Dataset(file_path='data', group='test', device=device, frame_count=frame_count, step_size=step_size)
 
     # if var folder not empty then load data and labels from saved numpy arrays
     # else create data and labels
-    if onlyOneFile == False:
-        if not os.path.exists('data/var'):
-            os.mkdir('data/var')
-        
-        # Check if data already saved in var folder 
+    if not os.path.exists('data/var'):
+        os.mkdir('data/var')
+    
+    # Check if data already saved in var folder 
 
-        if not os.path.exists('data/var/data_train.npy'):
-            training_data.create_data()
-            training_data.create_labels()
-            training_data.save_varibles('data/var/', 'data_train', 'label_train')
-        else:
-            print('Loading train data...')
-            training_data.load_variables('data/var/', 'data_train', 'label_train')
-            print('Train data loaded.')
-
-        if not os.path.exists('data/var/data_val.npy'):
-            val_data.create_data()
-            val_data.create_labels()
-            val_data.save_varibles('data/var/', 'data_val', 'label_val')
-        else:
-            print('Loading val data...')
-            val_data.load_variables('data/var/', 'data_val', 'label_val')
-            print('Val data loaded.')
-        
-        if not os.path.exists('data/var/data_test.npy'):
-            test_data.create_data()
-            test_data.create_labels()
-            test_data.save_varibles('data/var/', 'data_test', 'label_test')
-        else:
-            print('Loading test data...')
-            test_data.load_variables('data/var/', 'data_test', 'label_test')
-            print('Test data loaded.')
+    if not os.path.exists('data/var/data_train.pt'):
+        training_data.create_data()
+        training_data.create_labels()
+        training_data.save_varibles('data/var/', 'data_train', 'label_train')
     else:
-        training_data.create_one_data(file_train)
-        training_data.create_one_labels(file_train, 'train')
-        val_data.create_one_data(file_val)
-        val_data.create_one_labels(file_val, 'val')
-        test_data.create_one_data(file_test)
-        test_data.create_one_labels(file_test, 'test')
+        print('Loading train data...')
+        training_data.load_variables('data/var/', 'data_train', 'label_train')
+        print('Train data loaded.')
+
+    if not os.path.exists('data/var/data_val.pt'):
+        val_data.create_data()
+        val_data.create_labels()
+        val_data.save_varibles('data/var/', 'data_val', 'label_val')
+    else:
+        print('Loading val data...')
+        val_data.load_variables('data/var/', 'data_val', 'label_val')
+        print('Val data loaded.')
+    
+    if not os.path.exists('data/var/data_test.pt'):
+        test_data.create_data()
+        test_data.create_labels()
+        test_data.save_varibles('data/var/', 'data_test', 'label_test')
+    else:
+        print('Loading test data...')
+        test_data.load_variables('data/var/', 'data_test', 'label_test')
+        print('Test data loaded.')
     
 
 
     training_loader = DataLoader(training_data, batch_size=batch_size, shuffle=True)
     val_loader = DataLoader(val_data, batch_size=batch_size, shuffle=True)
-    test_loader = DataLoader(test_data, shuffle=True)
+    test_loader = DataLoader(test_data,batch_size=batch_size, shuffle=True)
 
-    # frame_features_train = training_loader.dataset.data[0].shape[1]
-    frame_features_train = training_loader.dataset.data.shape[1]
-
-    model = m.DilatedResidualLayer(channels=num_channel, output=1, dim=frame_features_train).to(device)
-    
+    model = m.DilatedResidualLayer(output_channels=num_class, input_channels=frame_count, filter_size=filter_size).to(device) # ? assigning batch size could be wrong
     loss_fn = nn.CrossEntropyLoss()
     optimizer = torch.optim.Adam(model.parameters(), lr=lr)
 
@@ -86,5 +72,5 @@ if __name__ == '__main__':
     trainer.train()
     print('Training complete.')
 
-    eval = eval.Eval(model, test_loader, device)
-    eval.eval()
+    # eval = eval.Eval(model, test_loader, device)
+    # eval.eval()
