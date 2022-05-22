23c194edeff6d4049327e3c72320f37ff9b07859
Giray Kilic <kilicgiray@outlook.com>
2022-05-22 16:23:59 +0300

    add sqeeze for model for compability

--- model.py
+++ model.py
@@ -13,10 +13,15 @@ class DilatedResidualLayer(nn.Module):
         self.conv_out = nn.Conv1d(filter_size, output_channels, 1)
 
     def forward(self, x):
-        out = self.conv_in(x)
+        out = x.unsqueeze(2)
+
+        out = self.conv_in(out)
         out = F.relu(self.conv_dilated(out))
         out = self.conv_1x1(out)
         out = self.dropout(out)
         out = self.conv_out(out)
+
+        out = out.squeeze(2)
+
         return out
         # return x + out # will be used when using multiple dilated layers
