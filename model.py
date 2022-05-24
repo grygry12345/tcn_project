f6cbaa328f87c2133fb19fc603fd82b5be1f4364
Giray Kilic <kilicgiray@outlook.com>
2022-05-24 10:54:22 +0300

    extend model SS_TCN

--- model.py
+++ model.py
@@ -1,27 +1,42 @@
-import torch
 import torch.nn as nn
 import torch.nn.functional as F
-
-# Temporal Convolutional Network dialutional layer
+import copy
 class DilatedResidualLayer(nn.Module):
-    def __init__(self, dilation=1, output_channels=1, input_channels=1, filter_size=4):
+    def __init__(self, dilation=1, num_output=1, num_input=2):
         super(DilatedResidualLayer, self).__init__()
-        self.conv_in = nn.Conv1d(input_channels, filter_size, 1)
-        self.conv_dilated = nn.Conv1d(filter_size, filter_size, 3, padding=dilation, dilation=dilation)
-        self.conv_1x1 = nn.Conv1d(filter_size, filter_size, 1)
+        self.conv_dilated = nn.Conv1d(num_input, num_output, 3, padding=dilation, dilation=dilation)
+        self.conv_1x1 = nn.Conv1d(num_output, num_output, 1)
         self.dropout = nn.Dropout()
-        self.conv_out = nn.Conv1d(filter_size, output_channels, 1)
+
+
+        self.shortcut = nn.Conv1d(num_input, num_output, 1)
+
+    def forward(self, x):
+                
+        residual = self.shortcut(x)
+        
+        out = F.relu(self.conv_dilated(x))
+        out = self.dropout(out)
+        out = self.conv_1x1(out) + residual
+
+        return out
+
+class SS_TCN(nn.Module): # 
+    def __init__(self, num_layers, filter_size, num_input, num_classes):
+        super(SS_TCN, self).__init__()
+        self.conv_1x1 = nn.Conv1d(num_input, filter_size, 1)
+        self.layers = nn.ModuleList([copy.deepcopy(DilatedResidualLayer(2 ** i, filter_size, filter_size)) for i in range(num_layers)])
+        self.conv_out = nn.Conv1d(filter_size, num_classes, 1)
 
     def forward(self, x):
         out = x.unsqueeze(2)
 
-        out = self.conv_in(out)
-        out = F.relu(self.conv_dilated(out))
         out = self.conv_1x1(out)
-        out = self.dropout(out)
-        out = self.conv_out(out)
+        for layer in self.layers:
+            out = layer(out)
+        
+        out = self.conv_out(out) 
 
         out = out.squeeze(2)
 
         return out
-        # return x + out # will be used when using multiple dilated layers
