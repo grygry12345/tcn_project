646c8cad76bb2744212a7cd38bf9f7fd6a1f00b7
Giray Kilic <kilicgiray@outlook.com>
2022-05-29 09:55:05 +0300

    add train_correct

--- trainer.py
+++ trainer.py
@@ -1,3 +1,4 @@
+from pyexpat import features
 import torch.nn as nn 
 import torch
 from torch.utils.data import DataLoader
@@ -5,7 +6,7 @@ from torch.utils.tensorboard import SummaryWriter
 
 class Trainer(nn.Module):
     def __init__(self, model, loss_fn, optimizer, epochs: int, train_dataloader: DataLoader, val_dataloader: DataLoader, \
-    test_dataloader: DataLoader, device: str = "cuda:0", writer: SummaryWriter = None):
+    device: str = "cpu", writer: SummaryWriter = None):
         super(Trainer, self).__init__()
         self.model = model
         self.loss_fn = loss_fn
@@ -13,38 +14,37 @@ class Trainer(nn.Module):
         self.epochs = epochs
         self.train_dataloader = train_dataloader
         self.val_dataloader = val_dataloader
-        self.test_dataloader = test_dataloader
         self.device = device
-        # Summary writer with date and model name
         self.writer = writer
     
 
     def _step_train(self):
-        train_loss = 0.0
+        train_loss, train_correct = 0.0, 0.0
         num_batches = len(self.train_dataloader)
+        size = len(self.train_dataloader.dataset)
         self.model.train()
-        for batch, (X, y) in enumerate(self.train_dataloader):
+        for _, (X, y) in enumerate(self.train_dataloader):
             X = X.to(self.device)
-            y = y.type(torch.FloatTensor).to(self.device)
+            y = y.type(torch.LongTensor).to(self.device)
 
 
             # Compute prediction error
             pred = self.model(X)
-
-            loss = self.loss_fn(pred, y.squeeze()) # ! fix this
+            loss = self.loss_fn(pred, y)
 
             # Backpropagation
             self.optimizer.zero_grad()
             loss.backward()
             self.optimizer.step()
-            
 
-
+            pred_target = pred.argmax(1)
+            train_correct += torch.sum(pred_target == y).item()
             train_loss += loss.item()
         
         train_loss /= num_batches
+        train_correct /= size
 
-        return train_loss
+        return train_loss, train_correct
             
     
     def _step_val(self):
@@ -56,19 +56,17 @@ class Trainer(nn.Module):
         with torch.no_grad():
             for _ , (X, y) in enumerate(self.val_dataloader):
                 X = X.to(self.device)
-                
-                y = y.type(torch.FloatTensor).to(self.device)
+                y = y.type(torch.LongTensor).to(self.device)
 
-                # Compute prediction error
                 pred = self.model(X)
                 
-                loss = self.loss_fn(pred, y.squeeze())
+                loss = self.loss_fn(pred, y)
 
                 val_loss += loss.item()
 
                 pred_target = pred.argmax(1)
 
-                val_correct += torch.sum(pred == y).item()
+                val_correct += torch.sum(pred_target == y).item()
 
 
         val_loss /= num_batches
@@ -79,10 +77,11 @@ class Trainer(nn.Module):
     def train(self):
         for t in range(self.epochs):
             print(f"Epoch {t}/{self.epochs}", end="\r")
-            train_loss = self._step_train()
+            train_loss, train_correct = self._step_train()
             val_loss, val_correct = self._step_val()
             self.writer.add_scalar('train_loss', train_loss, t)
             self.writer.add_scalar('val_loss', val_loss, t)
+            self.writer.add_scalar('train_acc', train_correct, t)
             self.writer.add_scalar('val_acc', val_correct, t)
         print()
 
