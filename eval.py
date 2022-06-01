4c00c641bee2c027d8b27c52edef2701b4ff6cf6
Giray Kilic <kilicgiray@outlook.com>
2022-06-01 22:26:32 +0300

    Add prediction for represent best and worst clips

--- eval.py
+++ eval.py
@@ -16,6 +16,69 @@ class Eval():
         self._preds = np.empty((0))
         self._gts = np.empty((0))
 
+
+        
+    def _find_best_worst_clips(self):
+        gts, preds = np.array([]), np.array([])
+        minCorrect = 999999
+        maxCorrect = 0
+
+        self.model.eval()
+        with torch.no_grad():
+            for _, (X, y) in enumerate(self.test_dataloader):
+                X = X.to(self.device)
+                y = y.type(torch.LongTensor).to(self.device)
+                ground_truth = y
+                ground_truth = ground_truth.cpu().numpy()
+
+                pred = self.model(X)
+                pred_target = pred.argmax(1)
+                pred_target = pred_target.cpu().numpy()
+
+                correct = np.sum(pred_target == ground_truth)
+                
+                if correct > maxCorrect:
+                    maxCorrect = correct
+                    best_pred = pred_target
+                    best_gts = ground_truth
+
+                if correct < minCorrect:
+                    minCorrect = correct
+                    worst_pred = pred_target
+                    worst_gts = ground_truth
+
+        return best_pred, best_gts, worst_pred, worst_gts, minCorrect, maxCorrect
+
+
+
+    def show_best_worst_clips(self):
+        best_pred, best_gts, worst_pred, worst_gts, minCorrect, maxCorrect = self._find_best_worst_clips()
+
+
+        # figure 1 only best clip
+        plt.figure(figsize=(20, 10))
+        plt.plot(best_pred, label='pred')
+        plt.plot(best_gts, label='gts')
+        plt.legend()
+        plt.xlabel('frames')
+        plt.ylabel('class')
+        plt.title('Best clip')
+        plt.savefig('best_clip.png') 
+
+        # figure 2 only worst clip
+        plt.figure(figsize=(20, 10))
+        plt.plot(worst_pred, label='pred')
+        plt.plot(worst_gts, label='gts')
+        plt.legend()
+        plt.xlabel('frames')
+        plt.ylabel('class')
+        plt.title('Worst clip')
+        plt.savefig('worst_clip.png')
+
+        print('Best clip accuracy: {}'.format(maxCorrect))
+        print('Worst clip accuracy: {}'.format(minCorrect))
+
+        
     def eval(self):
         self.model.eval()
         with torch.no_grad():
@@ -35,53 +98,6 @@ class Eval():
         f1 = f1_score(self._gts, self._preds, average='macro')
         precision = precision_score(self._gts, self._preds, average='macro')
         recall = recall_score(self._gts, self._preds, average='macro')
-
-        
-        return acc, f1, precision, recall
-
-    def _predict(self):
-        gts, preds = np.array([]), np.array([])
-
-        self.model.eval()
-        with torch.no_grad():
-            for _, (X, y) in enumerate(self.test_dataloader):
-                X = X.to(self.device)
-                y = y.type(torch.LongTensor).to(self.device)
-                ground_truth = y
-                ground_truth = ground_truth.cpu().numpy()
-
-                pred = self.model(X)
-                pred_target = pred.argmax(1)
-                pred_target = pred_target.cpu().numpy()
-
-                pred, ground_truth = self._expand_frames(pred_target, ground_truth)
-
-                # append to preds and gts
-                gts = np.append(gts, ground_truth)
-                preds = np.append(preds, pred)
-        
-        return gts, preds
-
-    def predict_show(self, start, end):
-        gts, preds = self._predict()
-        gts = gts[start:end]
-        preds = preds[start:end]
-
-        # matplotlib
-        plt.figure(figsize=(10, 5))
-        plt.plot(gts, label='ground truth')
-        plt.plot(preds, label='prediction')
-        plt.legend()
-        plt.show()                
-                
-    # exapnd frames by frame count
-    def _expand_frames(self, pred, gt):
-        pred = np.repeat(pred, self.frame_count)
-        gt = np.repeat(gt, self.frame_count)
-
-        return pred, gt
-
-
-        
             
+        return acc, f1, precision, recall
 
